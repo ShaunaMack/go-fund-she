f4333a81fda584b01d7c661b6123ee710f59da3c
Shauna Mack <shaunakatemack@gmail.com>
2024-12-11 17:16:30 +1000

    fix mixing import

--- animalgogo/users/views.py
+++ animalgogo/users/views.py
@@ -5,18 +5,16 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from projects.models import Project, Pledge
 from .serializers import CustomUserSerializer
+from django.contrib.auth import get_user_model
 
-# /users
-
+User = get_user_model()
 
 class CustomUserList(APIView):
-    # GET method - retrieve all users
     def get(self, request):
-        users = CustomUser.objects.all()
+        users = User.objects.all()
         serializer = CustomUserSerializer(users, many=True)
         return Response(serializer.data)
 
-    # POST method - create a new user
     def post(self, request):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
@@ -24,36 +22,30 @@ class CustomUserList(APIView):
             return Response(serializer.data)
         return Response(serializer.errors)
 
-# /users/<pk>
-
-
 class CustomUserDetail(APIView):
-
-    # helper method for getting a user and raising a 404 if that user does not exist
     def get_object(self, pk):
-        # try getting the user with the specified pk
         try:
-            return CustomUser.objects.get(pk=pk)
-        except CustomUser.DoesNotExist:
-            # raise an Http404 exception so that Django knows to show a 404
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
             raise Http404
 
-    # GET a single user's detail
     def get(self, request, pk):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
 
-    # PUT method – update user if the User has permission (authorization)
     def put(self, request, pk):
         user = self.get_object(pk)
         if not request.user.is_superuser and request.user.pk != user.pk:
-            return Response({"403: Forbidden. You are not authorized to update this user."}, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {"403: Forbidden. You are not authorized to update this user."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
         serializer = CustomUserSerializer(
             instance=user,
             data=request.data,
-            partial=True
+            partial=True,
         )
         if serializer.is_valid():
             serializer.save()
@@ -61,28 +53,25 @@ class CustomUserDetail(APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # DELETE method – delete user if the User has permission (authorization)
     def delete(self, request, pk):
         user = self.get_object(pk)
         if not request.user.is_superuser and request.user.pk != user.pk:
-            return Response({"403: Forbidden. You are not authorized to delete this user."}, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {"403: Forbidden. You are not authorized to delete this user."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
         user.delete()
         return Response({"200: User deleted successfully"}, status=status.HTTP_200_OK)
-
-
-# /users/me
 
 class MeDetail(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         user = request.user
-        # Fetch projects and pledges related to the user
         projects = Project.objects.filter(owner=user)
         pledges = Pledge.objects.filter(supporter=user)
 
-        # Serialize the data (update these serializers if necessary)
         project_data = [
             {
                 "id": project.id,
@@ -101,16 +90,20 @@ class MeDetail(APIView):
                 "amount": pledge.amount,
                 "anonymous": pledge.anonymous,
                 "comment": pledge.comment,
-                "project": {"id": pledge.project.id, "title": pledge.project.title},
+                "project": {
+                    "id": pledge.project.id,
+                    "title": pledge.project.title,
+                },
             }
             for pledge in pledges
         ]
 
-        return Response({
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
-            "projects": project_data,
-            "pledges": pledge_data,
-        })
-
+        return Response(
+            {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "projects": project_data,
+                "pledges": pledge_data,
+            }
+        )
